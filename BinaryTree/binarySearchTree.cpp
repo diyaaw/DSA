751b43b2ad49de61eb20b6aecb7ca1bb6f9e73eb
diyaaw <diya.wadhwa24@gmail.com>
2025-06-16 01:05:10 +0530

    commit

--- BinaryTree/binarySearchTree.cpp
+++ BinaryTree/binarySearchTree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 class Node{
     public:
@@ -11,64 +12,80 @@ class Node{
     }
 };
 
- Node* insert(Node *root,int target){
-    //base case
-    if(!root){
-        Node* temp=new Node(target);
-        return temp;
+static int idx=-1;
+Node* buildTree(vector<int> preorder){
+    if(preorder[idx]==-1){
+        return nullptr;
     }
-        if(root->data > target){
-            root->left=insert(root->left,target);
-        }
-        else{
-            root->right=insert(root->right,target);
-        }
-    
+    Node *root = new Node(preorder[idx]);
+    root->left=buildTree(preorder);//LEFT
+    root->right=buildTree(preorder);//RIGHT
+
     return root;
- }
+}
 
- //searching in bst
- bool search(Node* root, int target){
-    if(!root){
-        return 0;
-    }
-    if(root->data==target){
-        return 1;
-    }
+//  Node* insert(Node *root,int target){
+//     //base case
+//     if(!root){
+//         Node* temp=new Node(target);
+//         return temp;
+//     }
+//         if(root->data > target){
+//             root->left=insert(root->left,target);
+//         }
+//         else{
+//             root->right=insert(root->right,target);
+//         }
+    
+//     return root;
+//  }
 
-    if(root->data>target){
-        return search(root->left,target);
-    }
-    else{
-        return search(root->right,target);
-    }
+//  //searching in bst
+//  bool search(Node* root, int target){
+//     if(!root){
+//         return 0;
+//     }
+//     if(root->data==target){
+//         return 1;
+//     }
 
- }
+//     if(root->data>target){
+//         return search(root->left,target);
+//     }
+//     else{
+//         return search(root->right,target);
+//     }
 
- //inorder traversal
+//  }
 
- void inorder(Node* root){
-    if(!root){
-        return;
-    }
+//  //inorder traversal
 
-    //left side
-    inorder(root->left);
-    cout<<root->data<<" ";
-    inorder(root->right);
- }
+//  void inorder(Node* root){
+//     if(!root){
+//         return;
+//     }
+
+//     //left side
+//     inorder(root->left);
+//     cout<<root->data<<" ";
+//     inorder(root->right);
+//  }
 int main(){
-    int arr[]={6,3,17,5,11,18,2,1,20,14};
-    Node* root=nullptr;
 
-    for(int i=0;i<10;i++){
-        root=insert(root,arr[i]);
-    }
+    vector<int> preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root=buildTree(preorder);
+    cout<<root->data<<" ";
+//     int arr[]={6,3,17,5,11,18,2,1,20,14};
+//     Node* root=nullptr;
+
+//     for(int i=0;i<10;i++){
+//         root=insert(root,arr[i]);
+//     }
 
 
-    //Traversal
-    // inorder(root);
+//     //Traversal
+//     // inorder(root);
 
-    cout<<search(root,71);
-    return 0;
+//     cout<<search(root,71);
+//     return 0;
 }
